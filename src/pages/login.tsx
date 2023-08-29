566cd625275a2f8a4cc76c89456051173f4e4ca2
Benjamin Vredevelt <bjv753@gmail.com>
2023-08-29 17:07:40 -0500

    update

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,6 +6,8 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import { ThemedTitleV2 } from "@refinedev/mui";
 
+import { yariga } from '../assets'
+
 import { CredentialResponse } from "../interfaces/google";
 
 
@@ -30,7 +32,7 @@ export const Login: React.FC = () => {
             try {
                 window.google.accounts.id.initialize({
                     ux_mode: "popup",
-                    client_id: GOOGLE_CLIENT_ID,
+                    client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
                     callback: async (res: CredentialResponse) => {
                         if (res.credential) {
                             login(res);
